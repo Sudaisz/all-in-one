5b2c3645b3774db432d4cac9af65d4a4bb39cb11
Sudais main <sudaisabubakar6754@gmail.com>
2025-07-14 03:35:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 import os
 
-API_TOKEN = os.getenv("API_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+API_TOKEN = os.getenv("8138646021:AAEV8mZ-MFtpVgEN-_pFrRUAS4Qxt4fcCsQ")
+ADMIN_ID = int(os.getenv("6619154186"))
