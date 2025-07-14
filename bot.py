594bb8bd172caa227a6737ddfac508ee7f6292c4
Sudais main <sudaisabubakar6754@gmail.com>
2025-07-14 03:41:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,7 @@ import os
 
 API_TOKEN = os.getenv("8138646021:AAEV8mZ-MFtpVgEN-_pFrRUAS4Qxt4fcCsQ")
 ADMIN_ID = int(os.getenv("6619154186"))
+#start command
+@dp.message_handler(commands=['start'])
+async def start_cmd(message: types.Message):
+    await message.reply("Hello, I'm alive!")
